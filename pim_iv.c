0e064407b787d8c06f1bab065abd3c7aa6006b5b
Anderson Ramos 07 <andersonramos07@live.com>
2020-11-18 18:57:45 -0300

    Atualizando...

--- pim_iv.c
+++ pim_iv.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define ALPHA "\x1b[0m"
+#define GRAY "\e[0;37m"
+#define DGRAY "\e[1;30m"
 
 typedef enum { false, true } bool;
 
 char login [25]; char senha[25];
-int cont = 0;
+int count = 0;
 
-char nomep [25], emailp [50];
-char ruaav [50]; int ruanum; char bairro [25], cidade [25], uf [3]; int cep [9]; //registro do endereço
-int cpfp, telefonep, datap, datadp, febredorato; //"febredorato" é apenas pra não digitar aquele nome... hehe
+//REGISTRO DO PACIENTE
 
+char nomeP [25], cpfP [15], telefoneP [20], emailP [50]; //DADOS DE CONTATO
+char endereco [70], cep[9]; //DADOS DE ENDEREÇO
+char dataP, datadP; //DATAS
+int febredorato; //"febredorato" é apenas pra não digitar aquele nome... hehe
+
+//COMO IRÁ FUNCIONAR OS ID DAS PATOLOGIAS
 //fdr 1 = diabetes, 2 = obesidade, 3 = hipertensão, 4 = tuberculose, 5 = outros
 
 void boasVindas (void);
@@ -18,38 +30,80 @@ void confirmaLogin (void);
 
 void novoCadastro (void);
 
+void informeDP (void);
+
 int main(int argc, char **argv){
 
-    boasVindas();       
+    confirmaLogin();       
 
     return 0;
 }
 
 void boasVindas(){
+    system("clear");
     int resposta;
-    printf("\nSEJA BEM VIND@ AO SISTEMA DO PIM IV\n");
+    printf(GREEN "\nSEJA BEM VIND@ AO SISTEMA DO PIM IV\n" ALPHA);
 
-    printf("\nO que deseja fazer?\n\t1 - Acessar o Sistema:\n\t2 - Cadastrar novo acesso:\n\t...aperte qualquer outra tecla para sair!\nR: ");
+    printf("\nO que deseja fazer?\n\t\v 1 - Acessar o Sistema:\n\t 2 - Cadastrar novo acesso:\n\t 3 - Inserir dados do Paciente:\n\t ...aperte qualquer outra tecla para sair!\n\nR: ");
     scanf("%d", &resposta);
-        if(resposta==1){
-            confirmaLogin();
-        }
-        else if(resposta==2){
-            novoCadastro();
-        }
-        else{
-            exit(0);
+        switch(resposta){
+            case 1:
+                confirmaLogin();
+            break;
+            case 2:
+                novoCadastro();
+            break;
+            case 3:
+                informeDP();
+            break;
+            default:
+                printf("Favor selecione uma das opções!\n");
+                confirmaLogin();
         };
 }
 
 void confirmaLogin (){
-    printf("\n\t>>>>>>>teste de Acesso ao sistema<<<<<<<\n");
-    printf("\n\tRetornando para a tela de Boas-Vindas!<-------\n\t\t");
+    system("clear");
+    printf(RED "\n\t>>>>>>>ACESSO AO SISTEMA<<<<<<<\n" ALPHA);
+    printf("\v\nInsira o login: " DGRAY " --digita qualquer coisa--" ALPHA "\n\t");
+    scanf("%s", &login[0]);
+    printf("\v\nInsira a senha: " DGRAY " --relaxa que não tem senha nenhuma--" ALPHA "\n\t");
+    scanf("%s", &senha[0]);
+    printf("\n\tRetornando para a tela de Boas-Vindas!<-------\n\v");
     boasVindas();
 };
 
 void novoCadastro (){
-    printf("\n\t>>>>>>>teste de novo cadastro<<<<<<<\n");
-    printf("\n\tRetornando para a tela de Boas-Vindas!<-------\n\t\t");
+    system("clear");
+    printf(RED "\n\t>>>>>>>NOVO CADASTRO<<<<<<<\n" ALPHA);
+    printf("\v\nInsira o login: " DGRAY " --digita qualquer coisa--" ALPHA "\n\t");
+    scanf("%s", &login[0]);
+    printf("\v\nInsira a senha: " DGRAY " --digita uma senha qualquer--" ALPHA "\n\t");
+    scanf("%s", &senha[0]);
+    printf("\n\tRetornando para a tela de Boas-Vindas!<-------\n\v");
     boasVindas();
 };
+
+void informeDP (){
+    int cont = 0;
+    system("clear");
+    printf(BLUE "\v >>>Informe os dados do paciente: \n" ALPHA);
+        printf("\v\tNome: \n\t");
+        scanf("%s", &nomeP[cont]);
+        setbuf(stdin, NULL);
+        printf("\v\tCPF: \n\t");
+        scanf("%s", &cpfP[cont]);
+        setbuf(stdin, NULL);
+        printf("\v\tTelefone: \n\t");
+        scanf("%s", &telefoneP[cont]);
+        setbuf(stdin, NULL);
+        printf("\v\t Endereço completo: " DGRAY "Exemplo: Rua Dr. Ulisses, 23 - Vila UNIP, São Paulo - SP \n\t" ALPHA);
+        scanf("%s", &endereco[cont]);
+        setbuf(stdin, NULL);
+        printf("\n\t CEP: \n\t");
+        scanf("%s", &cep[cont]);
+        setbuf(stdin, NULL);             
+    printf(GREEN "\n\v Testado!\n\v" ALPHA);
+    cont++;
+    exit(0);
+}
