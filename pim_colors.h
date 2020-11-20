0b2c9bf1ed3a598524a6935c65018b840e6a894f
Anderson Ramos 07 <andersonramos07@live.com>
2020-11-20 15:14:06 -0300

    Um import de cores do sistema, modulando um header.

---
+++ pim_colors.h
@@ -0,0 +1,15 @@
+#ifndef pimcolors
+#define pimcolors
+
+#define STAMP "\x1b[46m"
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define ALPHA "\x1b[0m"
+#define YELLOW "\x1b[33m"
+#define GRAY "\e[0;37m"
+#define DGRAY "\e[1;30m"
+#define HIDDEN "\x1b[7m"
+
+#endif
