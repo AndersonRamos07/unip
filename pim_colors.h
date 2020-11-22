c3255130a9a78c3b0a8bee7789b2772dee14fef9
Anderson Ramos 07 <andersonramos07@live.com>
2020-11-22 11:00:45 -0300

    Cores inseridas no projeto principal, inativo

--- pim_colors.h
+++ pim_colors.h
@@ -1,19 +1,6 @@
 #ifndef pimcolors
 #define pimcolors
 
-#define STAMP "\x1b[41m"
-#define GSTAMP "\x1b[42m"
-#define YSTAMP "\x1b[43m"
-#define BSTAMP "\x1b[44m"
-
-#define RED "\x1b[31m"
-#define GREEN "\x1b[32m"
-#define BLUE "\x1b[34m"
-#define ALPHA "\x1b[0m"
-#define YELLOW "\x1b[33m"
-#define GRAY "\e[0;37m"
-#define DGRAY "\e[1;30m"
-#define HIDDEN "\x1b[7m"
-#define BLINK "\x1b[5m" 
+//cores inseridos no arquivo principal
 
 #endif
