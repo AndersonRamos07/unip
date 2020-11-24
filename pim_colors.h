d42defd8fb4a60b15b30a189d5694eacd10657c4
Anderson Ramos 07 <andersonramos07@live.com>
2020-11-24 19:49:42 -0300

    Arquivos inseridos para modular o programa, separando os headers, structures e functions.

--- pim_colors.h
+++ pim_colors.h
@@ -1,6 +1,34 @@
+/*
+*       *******       ///////
+*       COLORS
+*
+*       AROS-001 - STAMP COLORS
+*       AROS-002 - OTHR COLORS
+*
+*       *******       ///////
+*/
 #ifndef pimcolors
 #define pimcolors
 
-//cores inseridos no arquivo principal
+// AROS-001
+
+#define STAMP "\x1b[41m"
+#define GSTAMP "\x1b[42m"
+#define YSTAMP "\x1b[43m"
+#define BSTAMP "\x1b[44m"
+
+// AROS-002
+
+#define ALPHA "\x1b[0m"
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define YELLOW "\x1b[33m"
+#define GRAY "\e[0;37m"
+#define DGRAY "\e[1;30m"
+#define HIDDEN "\x1b[7m"
+#define BLINK "\x1b[5m"
+
+// END
 
 #endif
