b893cb55dd7723caecf1088c7b6842933d1340e1
Anderson Ramos 07 <andersonramos07@live.com>
2020-11-24 19:52:56 -0300

    Separando em módulos, através dos pim_colors.h, pim_functions.h e pim_functions.c

--- pim_iv.c
+++ pim_iv.c
@@ -1,251 +1,29 @@
+/*
+*       *******       ///////
+*       MAIN - CONTROLE PANDEMICO (COVID-19)
+*
+*       AROS-001 - HEADERS
+*       AROS-002 - MAIN
+*
+*       *******       ///////
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <curses.h>
 
-#define STAMP "\x1b[41m"
-#define GSTAMP "\x1b[42m"
-#define YSTAMP "\x1b[43m"
-#define BSTAMP "\x1b[44m"
+// AROS-001
 
-#define RED "\x1b[31m"
-#define GREEN "\x1b[32m"
-#define BLUE "\x1b[34m"
-#define ALPHA "\x1b[0m"
-#define YELLOW "\x1b[33m"
-#define GRAY "\e[0;37m"
-#define DGRAY "\e[1;30m"
-#define HIDDEN "\x1b[7m"
-#define BLINK "\x1b[5m"
+#include "pim_colors.h"
+#include "pim_functions.h"
 
-struct acesso
-{
-    char login[25];
-    char senha[7];
-};
-struct acesso sistema;
-
-struct ficha
-{
-    char nome[25], cpf, telefone, email, endereco[75];
-    char cep[9];
-    int diaN, mesN, anoN;
-    int diaD, mesD, anoD;
-    int doenca; //diabetes, obesidade, hipertensão, tuberculose, outros, mais de uma.
-};
-struct ficha paciente;
-
-void boasVindas(void);
-
-void cadastrarAcesso(struct acesso *ac);
-
-void confirmarAcesso(void);
-
-void cadastrarPaciente(struct ficha *fc);
-
-void retornarCadastro(void);
-
-// >>>>>>> PROGRAMA PRINCIPAL (main) <<<<<<<
+// AROS-002
 
 int main(int argc, char *argv[]){
 
     boasVindas();
 
-    //cadastrarPaciente(&paciente);
-
-   //confirmarAcesso();
-
-   //cadastrarAcesso(&sistema);
-
     return 0;
 };
 
-void boasVindas(void){
-    char aswer;
-    system("clear");
-    printf(STAMP "[  ||     =============================     ||  ]\n" ALPHA);
-    printf(STAMP "[::++::== CONTROLE PANDÊMICO (COVID-19) ==::++::]\n" ALPHA);
-    printf(STAMP "[  ||     =============================     ||  ]\n" ALPHA);
-    printf("\n * O QUE DESEJA FAZER?\n\t\v 1 - Acessar o Sistema:\n\t 2 - Cadastrar novo acesso:\n\t 3 - Protocolo de Fichas:\n\t\v ...aperte '0'(zero) para sair!\n\n * R: ");
-        do{
-            scanf("%c", &aswer);
-            switch (aswer)
-            {
-            case '1':
-                confirmarAcesso();
-                break;
-            case '2':
-                cadastrarAcesso(&sistema);
-                break;
-            case '0':
-                break;            
-            };
-            printf(YELLOW "Selecione uma opção válida! \n" ALPHA);
-        }while(aswer != '0');
-};
-
-void cadastrarAcesso(struct acesso *ac){
-    
-    FILE *pointer;
-
-    pointer = fopen("access.txt", "w");
-
-    if(pointer == NULL){
-        printf("Erro ao abrir o arquivo!");
-    }else{
-        system("clear");
-        printf(STAMP "[  ||     =============================     ||  ]\n" ALPHA);
-        printf(STAMP "[::++::== CONTROLE PANDÊMICO (COVID-19) ==::++::]\n" ALPHA);
-        printf(STAMP "[  ||     "BSTAMP"=====CADASTRO DE USUÁRIO====="ALPHA STAMP"     ||  ]\n" ALPHA);
-        printf("\n * Digite seu email: "DGRAY "--servirá como login de acesso-- \n\t" ALPHA);
-        scanf("%s", ac->login);
-        fflush(stdin);
-        printf("\n * Agora uma senha:" DGRAY "--deverá conter 5 digitos-- \n\t" ALPHA);
-        scanf("%s", ac->senha);
-        fprintf(pointer, "login:\t%s\nsenha:\t%s\n", ac->login, ac->senha);
-        fclose(pointer);
-        printf(YELLOW "\vNovos dados de acesso cadastrados com sucesso!\n\7"ALPHA);
-    };
-    exit(0);
-};
-
-void confirmarAcesso(void){
-    char logtmp[25];
-    char snhtmp[7];
-
-    char complogin[25];
-    char compsnh[7];
-
-    int checkoutL, checkoutS;
-
-    FILE *pointer;
-
-    pointer = fopen("access.txt", "r");
-
-    if(pointer == NULL){
-        printf("Erro ao abrir o arquivo!");
-    }else{
-        do{
-        system("clear");
-        printf(STAMP "[  ||     =============================     ||  ]\n" ALPHA);
-        printf(STAMP "[::++::== CONTROLE PANDÊMICO (COVID-19) ==::++::]\n" ALPHA);
-        printf(STAMP "[  ||     "GSTAMP"===========ACESSAR==========="ALPHA STAMP"     ||  ]\n" ALPHA);
-        printf("\n * Digite seu email: \n\t");
-        scanf("%s", logtmp);
-        fflush(stdin);
-        printf("\n * Digite sua senha: \n\t");
-        scanf("%s", snhtmp);
-        fscanf(pointer, "login:\t%s\nsenha:\t%s\n", complogin, compsnh);
-        fclose(pointer);
-
-        checkoutL = strcmp(logtmp, complogin);
-        checkoutS = strcmp(snhtmp, compsnh);
-
-        }while(checkoutL != 0 || checkoutS != 0);
-            cadastrarPaciente(&paciente);
-        };
-};
-
-void cadastrarPaciente(struct ficha *fc){
-    int idade = 0;
-    int tem = 0;
-
-    FILE *pointer;
-
-    pointer = fopen("fichas.txt", "a");
-    
-    if(pointer == NULL){
-        printf("Erro ao abrir o arquivo!");
-    }else{
-        system("clear");
-        fflush(stdin);
-        printf(STAMP "[  ||     =============================     ||  ]\n" ALPHA);
-        printf(STAMP "[::++::== CONTROLE PANDÊMICO (COVID-19) ==::++::]\n" ALPHA);
-        printf(STAMP "[  ||     "YSTAMP"=====CADASTRO DE PACIENTE===="ALPHA STAMP"     ||  ]\n" ALPHA);
-        printf("\nNome do paciente:....... \t");
-        fgets(paciente.nome, 25, stdin);
-        printf("\nCPF:....... \t");
-        fgets(&paciente.cpf, 25, stdin);
-        printf("\nTelefone:....... \t");
-        fgets(&paciente.telefone, 25, stdin);
-        printf("\nE-mail:....... \t");
-        fgets(&paciente.email, 25, stdin);
-        printf("\nEndereço:....... \t");
-        fgets(paciente.endereco, 75, stdin);
-        printf("\nCEP:....... \t");
-        scanf("%s", fc->cep);
-        fflush(stdin);
-            printf("\nData de nascimento:");
-            printf("\n....... Dia:");scanf("%d", &paciente.diaN);
-            printf("....... Mês:");scanf("%d", &paciente.mesN);
-            printf("....... Ano:");scanf("%d", &paciente.anoN);
-            fflush(stdin);
-            printf("\nData do diagnótico:");
-            printf("\n....... Dia:");scanf("%d", &paciente.diaD);
-            printf("....... Mês:");scanf("%d", &paciente.mesD);
-            printf("....... Ano:");scanf("%d", &paciente.anoD);
-            printf("\nO paciente possui alguma dessas doenças?\n Se sim, digite o número correspondente:");
-            printf("\n\t\v-Diabetes?(1)\n\t-Obesidade?(2)\n\t-Hipertensão?(3)\n\t-Tuberculose?(4)\n\t-Outras?(5)\n\t-Mais de uma?(6)\n\v\t Digite '0'(zero) Caso negativo!\n\nR: ");
-                scanf("%d", &tem);
-                    switch (tem)
-                    {
-                        case 1:
-                            tem = 1;
-                            break;
-                        case 2:
-                            tem = 2;
-                            break;
-                        case 3:
-                            tem = 3;
-                            break;
-                        case 4:
-                            tem = 4;
-                            break;
-                        case 5:
-                            tem = 5;
-                            break;
-                        case 6:
-                            tem = 6;
-                            break;
-                        case 0:
-                            tem = 0;
-                            break;
-                        default:
-                            printf(YELLOW "Favor clicar em uma das opções válidas!\n" ALPHA);
-                    };
-            idade = (paciente.anoD - paciente.anoN);
-            if(idade >= 65 || tem > 0){
-                printf(RED "\v *** PACIENTE PERTENCE AO GRUPO DE RISCO *** \n" ALPHA);
-                fprintf(pointer, "CEP:\t%s\nIdade:\t%d anos\n\n", fc->cep, idade);
-                fclose(pointer);
-                printf(YELLOW"ARQUIVO GERADO PARA ENVIO À SECRETARIA DE SAÚDE\n" ALPHA);
-            }else{
-                printf(YELLOW "\vNovos dados de acesso cadastrados com sucesso!\n\7"ALPHA);
-            };
-             //   retornarCadastro();        
-        };
-    exit(0);
-};
-
-/*
-void retornarCadastro(){
-    int choice;
-    fflush(stdin);
-    system("clear");
-    printf("Deseja realizar mais um cadastro?\n");
-    printf("\v\t- Sim (Digite '1' um);\v\t- Não (Digite '0' zero);\n");
-    scanf("%d", &choice);
-    switch (choice)
-    {
-    case 0:
-        boasVindas();
-        break;
-    case 1:
-        cadastrarPaciente(&paciente);
-        break;
-    default:
-        printf("Escolha uma opção válida!");
-        break;
-    };
-}
-*/
+// END
